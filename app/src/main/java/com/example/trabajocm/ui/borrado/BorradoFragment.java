97253998f95df1789392d7700c273f43cb335ff7
ricardovrodri <ricardovaz2000@gmail.com>
2024-04-10 10:28:57 +0200

    Edit y Borrado

--- app/src/main/java/com/example/trabajocm/ui/borrado/BorradoFragment.java
+++ app/src/main/java/com/example/trabajocm/ui/borrado/BorradoFragment.java
@@ -1,20 +1,25 @@
 package com.example.trabajocm.ui.borrado;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.trabajocm.MainActivity;
+import com.example.trabajocm.R;
 import com.example.trabajocm.databinding.FragmentBorradoBinding;
 
 public class BorradoFragment extends Fragment {
 
     private FragmentBorradoBinding binding;
+
+    private ImageView imageView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +28,19 @@ public class BorradoFragment extends Fragment {
 
         binding = FragmentBorradoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        imageView = root.findViewById(R.id.imagen_cargada_borrado);
+
+        if (getActivity() instanceof MainActivity) {
+            MainActivity mainActivity = (MainActivity) getActivity();
+            Uri myUri = mainActivity.selectedImageUri;
+            if(myUri!=null){
+                imageView.setImageURI(myUri);
+            }else{
+                imageView.setImageResource(R.drawable.imagen_sin_cargar);
+            }
+
+        }
 
         //final TextView textView = binding.textGallery;
         //galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
--- app/src/main/java/com/example/trabajocm/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/trabajocm/ui/slideshow/SlideshowFragment.java
@@ -1,19 +1,33 @@
 package com.example.trabajocm.ui.slideshow;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import com.example.trabajocm.MainActivity;
+import com.example.trabajocm.R;
 import com.example.trabajocm.databinding.FragmentSlideshowBinding;
+
+import java.io.InputStream;
+
 
 public class SlideshowFragment extends Fragment {
 
+    private ImageView imageView;
     private FragmentSlideshowBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,6 +37,20 @@ public class SlideshowFragment extends Fragment {
 
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        imageView = root.findViewById(R.id.imagen_cargada_edit);
+
+        if (getActivity() instanceof MainActivity) {
+            MainActivity mainActivity = (MainActivity) getActivity();
+            Uri myUri = mainActivity.selectedImageUri;
+            if(myUri!=null){
+                String imageUriString = myUri.toString();
+                displayImage(myUri, imageUriString);
+            }else{
+                imageView.setImageResource(R.drawable.imagen_sin_cargar);
+            }
+
+        }
 
         //final TextView textView = binding.textSlideshow;
         //slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
@@ -34,4 +62,39 @@ public class SlideshowFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    private void displayImage(Uri imageUri, String tmp) {
+        try {
+            // Cargar imagen en el ImageView
+            imageView.setImageURI(imageUri);
+            // Obtener el ContentResolver para acceder a los datos del proveedor de contenido
+            //ContentResolver contentResolver = requireActivity().getContentResolver();
+
+            // Leer metadatos de la imagen utilizando metadata-extractor
+            //InputStream inputStream = contentResolver.openInputStream(imageUri);
+            //if (inputStream != null) {
+                //Metadata metadata = ImageMetadataReader.readMetadata(inputStream);
+
+                // Construir una cadena con los metadatos
+                //StringBuilder metadataString = new StringBuilder("METADATOS::\n\n");
+
+                //for (Directory directory : metadata.getDirectories()) {
+                    //for (Tag tag : directory.getTags()) {
+                        //Log.d("asd", tag.getTagName());
+                        //metadataString.append(tag.getTagName()).append(": ").append(tag.getDescription()).append("\n");
+
+                    //}
+                //}
+
+
+
+                // Mostrar metadatos en TextView
+                //metadataTextView.setText(metadataString.toString());
+                //inputStream.close();
+            //}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
